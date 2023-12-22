9730f5e324f9d271b4682ebd8492fe86067b5e81
malsuke <sukemal096@gmail.com>
2023-12-23 01:43:11 +0900

    chore

---
+++ api/docker/container/container_test.go
@@ -0,0 +1,16 @@
+package container
+
+// func TestNewContainerService(t *testing.T) {
+// 	testID := "test-container-id"
+// 	c, err := NewContainerService(&testID)
+
+// 	if err != nil {
+// 		t.Errorf("NewContainerService returned an error: %v", err)
+// 	}
+
+// 	log.Debug().Str("container id", c.Id)
+
+// 	if c.Id != testID {
+// 		t.Errorf("Expected ContainerService ID to be '%s', got '%s'", testID, c.Id)
+// 	}
+// }
--- api/docker/handler/create.go
+++ api/docker/handler/create.go
@@ -1,7 +1,50 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	. "docker-api/api/docker/container"
+
+	. "github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	ssh = []*ContainerService{
+		NewContainerWithConfig(
+			&Config{
+				Image: "ubuntu:latest",
+			},
+			&HostConfig{
+				PortBindings: nat.PortMap{
+					"22/tcp": []nat.PortBinding{
+						{
+							HostPort: "0",
+						},
+					},
+				},
+			},
+			nil,
+			nil,
+		),
+		NewContainerWithConfig(
+			&Config{
+				Image: "ubuntu:latest",
+			},
+			&HostConfig{
+				PortBindings: nat.PortMap{
+					"22/tcp": []nat.PortBinding{
+						{
+							HostPort: "0",
+						},
+					},
+				},
+			},
+			nil,
+			nil,
+		),
+	}
+)
 
 func CreateContainer(c echo.Context) error {
-	return nil
+
 }
