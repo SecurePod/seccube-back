50eb83fa9ec1e9db0af7a3053674871bf9bc7405
malsuke <sukemal096@gmail.com>
2023-12-23 01:43:26 +0900

    add: 複数コンテナ起動テストを追加

---
+++ api/docker/container/createmulti_test.go
@@ -0,0 +1,64 @@
+package container
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	. "github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
+)
+
+var (
+	ssh = []*ContainerService{
+		NewContainerWithConfig(
+			&Config{
+				Image: "ubuntu:latest",
+				Cmd:   []string{"/bin/bash"},
+				Tty:   true,
+			},
+			&HostConfig{
+				PortBindings: nat.PortMap{
+					"22/tcp": []nat.PortBinding{
+						{
+							HostPort: "0",
+						},
+					},
+				},
+			},
+			nil,
+			nil,
+		),
+		NewContainerWithConfig(
+			&Config{
+				Image: "ubuntu:latest",
+				Cmd:   []string{"/bin/bash"},
+				Tty:   true,
+			},
+			&HostConfig{
+				PortBindings: nat.PortMap{
+					"22/tcp": []nat.PortBinding{
+						{
+							HostPort: "0",
+						},
+					},
+				},
+			},
+			nil,
+			nil,
+		),
+	}
+)
+
+func TestCreateMultiple(t *testing.T) {
+	ctx := context.Background()
+	for _, container := range ssh {
+		t.Run("create container", func(t *testing.T) {
+			id, err := container.CreateContainer(ctx, cli)
+			fmt.Println(id)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
